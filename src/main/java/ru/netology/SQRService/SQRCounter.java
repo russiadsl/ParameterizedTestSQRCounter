1d752624547db24853d48325712d80143ed35d91
russiadsl <bezugly_spb@mail.ru>
2021-12-21 00:30:17 +0300

    Rework SQRTests

--- src/main/java/ru/netology/SQRService/SQRCounter.java
+++ src/main/java/ru/netology/SQRService/SQRCounter.java
@@ -1,7 +1,7 @@
 package ru.netology.SQRService;
 
 public class SQRCounter {
-    public int sqrcounter(int start, int end) {
+    public int sqrCounter(int start, int end) {
         int counter = 0;
         for (int i = 10; i <= 99; i++) {
             if (i * i >= start && i * i <= end) {
--- src/test/java/ru/netology/SQRService/SQRCounterTest.java
+++ src/test/java/ru/netology/SQRService/SQRCounterTest.java
@@ -10,11 +10,10 @@ class SQRCounterTest {
     @ParameterizedTest
     @CsvSource (value = {"'sqrcounter0_100', 0, 100, 1",
             "'sqrcounter100_900', 100, 900, 21",
-            "'sqrcounter899_901', 899, 901, 1",
-            "'sqrcounter0_100 but 0', 0, 100, 0"})
-    void sqrcounter(String name, int start, int end, int expected) {
+            "'sqrcounter899_901', 899, 901, 1"})
+    void shouldCountSqr(String name, int start, int end, int expected) {
         SQRCounter service = new SQRCounter();
-        int actual = service.sqrcounter(start, end);
+        int actual = service.sqrCounter(start, end);
         assertEquals(expected, actual);
     }
 }
